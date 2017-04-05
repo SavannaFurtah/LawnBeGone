e0db41719dc9e6ec669bc9695c82950b0ea7a47a
Savanna-Rae Furtah <savannarae@live.ca>
2017-04-05 13:46:17 -0400

    still playing with xhtml pages

--- src/java/Lawn/LawnCutterData.java
+++ src/java/Lawn/LawnCutterData.java
@@ -14,11 +14,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
 
 /**
  *
  * @author c0656308
  */
+@Named
+@SessionScoped
+@ManagedBean
 public class LawnCutterData {
     private List<LawnCutter> cutter = new ArrayList<>();
     private LawnCutter currentCutter;
