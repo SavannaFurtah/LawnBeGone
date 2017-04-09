58759356657d1dac2dca9c57e2c5d697e4e609fe
Savanna-Rae Furtah <savannarae@live.ca>
2017-04-09 15:11:10 -0400

    added add a job page

--- src/java/Lawn/UserData.java
+++ src/java/Lawn/UserData.java
@@ -63,7 +63,7 @@ public class UserData {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         if (ul.verifyCredentials(currentUser.getEmail(), currentUser.getHashedPassword())) {
             loggedIn = true;
-            return "index";
+            return "HeaderBar";
         }
         facesContext.addMessage("loginForm", new FacesMessage("Username or Password is incorrect."));
         return null;
